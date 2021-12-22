db8254e3d42888637a6d77854d65cce99871af8e
gruzdev.m <gruzdev.m@myseldon.com>
2021-12-22 15:54:55 +0300

    Подключение к Redis и RabbitMq

---
+++ MySampleApp/Controllers/QueueController.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace MySampleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueueController : ControllerBase
+    {
+        private string _queueName;
+
+        [HttpPost]
+        [Route("AddMessage")]
+        public void AddMessageToQueue(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                _queueName = "main";
+                channel.QueueDeclare(queue: _queueName,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: _queueName,
+                    basicProperties: null,
+                    body: body);
+            }
+        }
+    }
+}
